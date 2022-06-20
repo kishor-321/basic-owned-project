2143c933412e67aaf65fd32f53f6889996a1ef80
kishor-321 <kishordeshmane321@gmail.com>
2022-06-20 18:46:00 +0530

    Basic Project

---
+++ src/test/java/org/testRunners/MyTestRunner.java
@@ -0,0 +1,28 @@
+package org.testRunners;
+
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.testng.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/org/features/FlipkartSearchPage.feature",
+		glue = {"stepDefinitions","org/applicationHooks"},
+		monochrome=true,
+		dryRun = false,
+		//tags="@Smoke or @Regression",
+		plugin = {"pretty",
+			"html:target/html_report/cucumber_reports.html",
+			"junit:target/junit_report/junit_reports.xml",
+			"json:target/json_report/json_reports.json",
+			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+			}
+//		format = {"pretty","html:report"} //for older application
+		)
+public class MyTestRunner { //extends AbstractTestNGCucumberTests {
+	
+}
+//Extent 1: new html create-> htmlreport_date.html
+///automatic create html report
